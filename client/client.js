d7fd91af2c40478fc2a3a0847ed4f427d4b044af
KovaKreative <eric.kovalevskyy@gmail.com>
2023-03-16 08:30:11 -0400

    Initialized project

---
+++ client/client.js
@@ -0,0 +1,4 @@
+// Connect to server
+// Request the file (from argv)
+// Listen to when server sends data
+// Download the data
---
+++ server/server.js
@@ -0,0 +1,5 @@
+// Start server
+// Connect client
+// Listen for file request
+// Look in the folder to check for file
+// Send the contents of the file to client
